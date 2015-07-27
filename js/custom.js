b0d3d8e2d7fea7639ad5b9fb241cea2d35dfa9cd
daschy <miloertola@gmail.com>
2015-07-28 00:56:36 +0200

    fix

--- js/custom.js
+++ js/custom.js
@@ -3,12 +3,51 @@
 //     'use strict';
 
 
-    $(document).ready(function() {
-    	
-        document.onSubmit = function(evt) {
-            console.log(evt);
-            return false;
-        };
+$(document).ready(function() {
+
+
+
+
+    $('form').submit(function(e) {
+    	var $contactForm = $('form');
+
+        e.preventDefault();
+
+        $.ajax({
+            url: "//formspree.io/askme@redbabel.com",
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json",
+            beforeSend: function() {
+            	$contactForm.find('.alert').addClass('hidden');
+            	$contactForm.find('.alert--loading').toggleClass('hidden');
+            },
+            success: function(data) {
+                $contactForm.find('.alert--loading').toggleClass('hidden');
+                $contactForm.find('.alert--success').toggleClass('hidden');
+            },
+            error: function(err) {
+                $contactForm.find('.alert--loading').toggleClass('hidden');
+                $contactForm.find('.alert--error').toggleClass('hidden');
+            }
+        });
+        return false;
     });
+
+
+    // document.onSubmit = function() {
+    //     var text = $('textarea').val();
+    //     console.log(text);
+    //     $.ajax({
+    //         url: "//formspree.io/askme@redbabel.com",
+    //         method: "POST",
+    //         data: {
+    //             message: $('textarea').val()
+    //         },
+    //         dataType: "json"
+    //     });
+    //     return true;
+    // };
+});
 
 // })
