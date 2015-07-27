d53f580d25b66cd800c07bb20b29fd4e02cfad42
daschy <miloertola@gmail.com>
2015-07-27 23:44:45 +0200

    fix

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+// (function(window) {
+
+//     'use strict';
+
+
+    $(document).ready(function() {
+    	
+        document.onSubmit = function(evt) {
+            console.log(evt);
+            return false;
+        };
+    });
+
+// })
